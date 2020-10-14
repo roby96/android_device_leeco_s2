a36074b312b8024d1b70bf479ae8441b90b28193
alonsoj636 <alonsojimenez.jm3@gmail.com>
2020-10-14 07:56:30 +0000

    s2: Nuke Fingerprint

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -64,10 +64,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.leeco_s2
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
