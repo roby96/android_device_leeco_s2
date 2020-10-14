3b85db6f80d42b1e78624234129697206090fc20
alonsoj636 <alonsojimenez.jm3@gmail.com>
2020-10-14 07:25:19 +0000

    s2: Arrow-OS Bringup

--- arrow_s2.mk
+++ arrow_s2.mk
@@ -17,14 +17,13 @@
 # Inherit device configuration
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common ArrowOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2
-PRODUCT_NAME := aosp_s2
+PRODUCT_NAME := arrow_s2
 PRODUCT_BRAND := LeEco
 PRODUCT_MANUFACTURER := LeMobile
 
